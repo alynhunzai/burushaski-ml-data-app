96d133f0e4f4c42e982a5658b3a61fdc7565dd5c
Noor Ali <nuraly211@gmail.com>
2026-08-07 17:46:44 +0000

    refactor(eslint.config.js): update configuration to use latest ECMAScript features and module syntax

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,26 +1,19 @@
-import { configs } from "@eslint/js";
-import { rules as _rules } from "eslint-config-google";
+import js from "@eslint/js";
 import globals from "globals";
 
 export default [
-  configs.recommended,
+  js.configs.recommended,
   {
     files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: 2018,
-      sourceType: "commonjs",
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
-        // Equates to env: { node: true, es6: true }
-        process: "readonly",
-        require: "readonly",
-        module: "readonly",
-        __dirname: "readonly",
         ...globals.browser,
         ...globals.node,
       },
     },
     rules: {
-      ..._rules,
       "no-restricted-globals": ["error", "name", "length"],
       "prefer-arrow-callback": "error",
       "quotes": ["error", "double", {"allowTemplateLiterals": true}],
